3568ce061a0895c623311bb396221c90da788458
Anishmehta8210 <anishmehta7782@gmail.com>
2023-03-16 16:19:01 +0530

    first commit

---
+++ post_portal/models.py
@@ -0,0 +1,53 @@
+from django.db import models
+
+# Create your models here.
+
+class UserMaster(models.Model):
+    email = models.EmailField(max_length=50)
+    password = models.CharField(max_length=50)
+    otp = models.IntegerField()
+    role = models.CharField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_verified = models.BooleanField(default=False)
+    is_created = models.DateTimeField(auto_now_add=True)
+    is_updated = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return self.email
+
+     
+
+
+
+class Candidate(models.Model):
+    user_id = models.ForeignKey(UserMaster,on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    contact = models.CharField(max_length=50)
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    address = models.CharField(max_length=150)
+    dob = models.CharField(max_length=150)
+    gender = models.CharField(max_length=150)
+    profile_pic = models.ImageField(upload_to="images/candidate",null=True,blank=True)
+
+    def __str__(self):
+       return self.firstname
+
+class Company(models.Model):
+    user_id = models.ForeignKey(UserMaster,on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=50)
+    lastname = models.CharField(max_length=50)
+    company_name = models.CharField(max_length=150)
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=50)
+    contact = models.CharField(max_length=50)
+    address = models.CharField(max_length=150)
+    logo_pic = models.ImageField(upload_to="images/company",null=True,blank=True)
+
+    def __str__(self):
+        return self.firstname
+    
+    
+    
